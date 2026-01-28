33b3d3bfc8d2ea594a81d15bf21d010aa0e74fcb
Amaro-peter <ph_amaro@id.uff.br>
2026-01-28 14:58:37 -0300

    fix: prisma client import

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,6 +1,6 @@
 import { env } from '@env/index'
 import { adapter } from './helper/adapter'
-import { PrismaClient } from '@prisma/client/extension'
+import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient({
   adapter,
