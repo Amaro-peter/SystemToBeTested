4e4a75cedd75cdd520543f49ed73b7f533178948
Amaro-peter <ph_amaro@id.uff.br>
2026-01-28 13:46:36 -0300

    feat: adding dependencies and prisma updated

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,6 +1,8 @@
 import { env } from '@env/index'
-import { PrismaClient } from '@prisma/client'
+import { adapter } from './helper/adapter'
+import { PrismaClient } from '@prisma/client/extension'
 
 export const prisma = new PrismaClient({
+  adapter,
   log: env.LOG_LEVEL === 'debug' ? ['query', 'info', 'warn'] : [],
 })
