d40aa2ea2648d29403cee109d7ecebfab13df536
Amaro-peter <ph_amaro@id.uff.br>
2026-01-28 18:07:27 -0300

    fix: running format

--- src/lib/prisma/helpers/adapter.ts
+++ src/lib/prisma/helpers/adapter.ts
@@ -1,9 +1,9 @@
-import { env } from "@env/index";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { env } from '@env/index'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 const pool = new Pool({
-    connectionString: env.DATABASE_URL,
-});
+  connectionString: env.DATABASE_URL,
+})
 
-export const adapter = new PrismaPg(pool);
+export const adapter = new PrismaPg(pool)
